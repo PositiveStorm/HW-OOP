b77ab8fa48018ecef8a3c526a318b1cfc96d3212
PositiveStorm <polinasabirova16.04@gmail.com>
2022-03-17 08:51:25 +0300

    Method rate.lecturer added to class Student

--- main.py
+++ main.py
@@ -6,7 +6,16 @@ class Student:
         self.finished_courses = []
         self.courses_in_progress = []
         self.grades = {}
+        self.rate = {}
 
+    def rate_lecturer(self, lecturer, course, rate):
+        if isinstance(lecturer, Lecturer) and course in self.courses_in_progress and course in lecturer.courses_in_progress:
+            if course in lecturer.rate:
+                lecturer.rate[course] += [rate]
+            else:
+                lecturer.rate[course] = [rate]
+        else:
+            return 'Ошибка'
 
 class Mentor:
     def __init__(self, name, surname):
@@ -14,20 +23,17 @@ class Mentor:
         self.surname = surname
         self.courses_attached = []
 
-    # def rate_hw(self, student, course, grade):
-    #     if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
-    #         if course in student.grades:
-    #             student.grades[course] += [grade]
-    #         else:
-    #             student.grades[course] = [grade]
-    #     else:
-    #         return 'Ошибка'
+
 
 class Lecturer(Mentor):
     def __init__(self, name, surname):
         self.name = name
         self.surname = surname
         self.courses_attached = []
+        self.courses_in_progress = []
+        self.rate = {}
+
+
 
 class Reviewer(Mentor):
     def __init__(self, name, surname):
@@ -47,14 +53,25 @@ class Reviewer(Mentor):
 
 
 
-best_student = Student('Ruoy', 'Eman', 'your_gender')
-best_student.courses_in_progress += ['Python']
+# best_student = Student('Ruoy', 'Eman', 'your_gender')
+# best_student.courses_in_progress += ['Python']
+#
+# cool_reviewer = Reviewer('Some', 'Buddy')
+# cool_reviewer.courses_attached += ['Python']
+#
+# cool_reviewer.rate_hw(best_student, 'Python', 10)
+# cool_reviewer.rate_hw(best_student, 'Python', 7)
+# cool_reviewer.rate_hw(best_student, 'Python', 10)
+#
+# print(best_student.grades)
 
-cool_mentor = Reviewer('Some', 'Buddy')
-cool_mentor.courses_attached += ['Python']
+good_student = Student('Ruoy', 'Eman', 'your_gender')
+good_student.courses_in_progress += ['Python']
 
-cool_mentor.rate_hw(best_student, 'Python', 10)
-cool_mentor.rate_hw(best_student, 'Python', 10)
-cool_mentor.rate_hw(best_student, 'Python', 10)
+cool_lecturer = Lecturer('Some', 'Buddy')
+cool_lecturer.courses_in_progress += ['Python']
 
-print(best_student.grades)
+good_student.rate_lecturer(cool_lecturer, 'Python', 4)
+good_student.rate_lecturer(cool_lecturer, 'Python', 3)
+good_student.rate_lecturer(cool_lecturer, 'Python', 1)
+print(cool_lecturer.rate)
